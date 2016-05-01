4fff564e12d51120aea24af0b1b2e28011b3a983
srikolabalaji <srikolabalaji@yahoo.com>
2016-05-01 17:11:27 +0530

    Adding HelloJava2

--- LearningJava/LearningJava/src/com/learningjava/HelloJava.java
+++ LearningJava/LearningJava/src/com/learningjava/HelloJava.java
@@ -19,9 +19,6 @@ public class HelloJava {
 		// TODO Auto-generated method stub
 		JFrame jf = new JFrame("Learning Java");
 		JLabel jl = new JLabel("Welcome to Learning Java",JLabel.CENTER);
-		
-		
-		jf.add(new HelloComponent());
 		jf.add(jl);
 		jf.setSize(600, 600);
 		jf.setVisible(true);
